acaf1cb8439cf2503a5ead43e119e78dc04023d5
MelihOrel <36665925+MelihOrel@users.noreply.github.com>
2023-03-08 13:13:53 +0300

    Add files via upload

---
+++ theGower.R
@@ -0,0 +1,24 @@
+## Data set sample 
+sex <- c('E','F','E','F','F')
+blood_pres <- c(140,120,160,150,140 )
+pain_level <- c(5,3,1,4,1)
+test_1 <- sample(c('Positive','Negative'),5,replace = T)
+test_2 <- sample(c('Positive','Negative'),5,replace = T)
+test_3 <- sample(c('Positive','Negative'),5,replace = T)
+
+df <- data.frame(sex,blood_pres,pain_level,test_1,test_2,test_3) ; head(df)
+
+
+###############################################
+library(cluster)
+
+distdf<-daisy(df, metric="gower", type = list(ordratio=c(3),symm = c(1),asymm = c(4:6)),
+      stand=TRUE)
+
+clust_gower <- hclust(distdf,method = 'complete') # single model
+
+plot(clust_gower)
+
+rect.hclust(clust_gower,k = 2)
+
+cut_model1 <- (cutree(clust_gower, 2))
